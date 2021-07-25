71d43d9d8f6b8f52f8a9d9b898e8f4f755b2c94a
fmolliet <fmolliet@hotmail.com>
2021-07-24 23:04:20 -0300

    Correção no docker file e deploy via esteira heroku

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,11 @@ WORKDIR /app
 
 COPY .mvn/ .mvn
 COPY mvnw pom.xml ./
-# RUN ./mvnw package -Dmaven.test.skip=true
-CMD ["./mvnw", "-Dmaven.test.skip=true"]
+CMD ["./mvnw","install", "-Dmaven.test.skip=true"]
+CMD ["./mvnw","package", "-Dmaven.test.skip=true"]
 
 ARG JAR_FILE=target/*.jar 
 COPY /target/producers-*.jar /producers.jar
 EXPOSE 8080
 
-ENTRYPOINT ["java","-Xmx521m", "-Dserver.port=${PORT}" ,"-jar","/producers.jar"]
+ENTRYPOINT ["java","-Xmx521m" ,"-jar","/producers.jar"]
--- src/main/java/br/com/tomatch/producers/ProducersApplication.java
+++ src/main/java/br/com/tomatch/producers/ProducersApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableCaching
-// @EnableSwagger2
+@EnableSwagger2
 // @EnableWebSecurity
 public class ProducersApplication {
 
